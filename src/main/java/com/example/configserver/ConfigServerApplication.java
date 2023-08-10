540dfd47c1d46be5c3025c9a34420e268a49519b
Suyog Shinde <Suyog Shindesuyog.shinde@appliedaiconsulting.com>
2023-08-10 17:09:39 +0530

    config server docker file

--- src/main/java/com/example/configserver/ConfigServerApplication.java
+++ src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
-
+public class ConfigServer {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServer.class, args);
+  }
 }
