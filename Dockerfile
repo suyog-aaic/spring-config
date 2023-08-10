5da86cc5538ce3a84371d905c48760baee42cd9d
Suyog Shinde <Suyog Shindesuyog.shinde@appliedaiconsulting.com>
2023-08-10 15:08:22 +0530

    config server docker file

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM maven:3.6.3-jdk-11 AS build
+WORKDIR /app
+COPY pom.xml .
+RUN mvn dependency:go-offline
+COPY src ./src
+RUN mvn package -DskipTests
+FROM openjdk:11-jre
+WORKDIR /app
+COPY --from=build /app/target/*.jar /app/app.jar
+EXPOSE 8888
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
+
+
