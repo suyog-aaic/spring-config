66cb84731b645aa110fe0b006d25301cb2be50b2
Suyog Shinde <Suyog Shindesuyog.shinde@appliedaiconsulting.com>
2023-08-11 15:49:03 +0530

    code changes

--- src/main/java/com/example/configserver/ConfigServer.java
+++ src/main/java/com/example/configserver/ConfigServer.java
@@ -21,3 +21,20 @@ public class ConfigServer {
     SpringApplication.run(ConfigServer.class, args);
   }
 }
+
+@RestController
+@RequestMapping("/api/vault")
+public class VaultController {
+
+    private final VaultTemplate vaultTemplate;
+
+    public VaultController(VaultTemplate vaultTemplate) {
+        this.vaultTemplate = vaultTemplate;
+    }
+
+    @PostMapping("/secrets")
+    public ResponseEntity<String> addSecret(@RequestBody Map<String, String> secret) {
+        vaultTemplate.write("secret/data/myapp", secret);
+        return ResponseEntity.ok("Secret added successfully");
+    }
+}
