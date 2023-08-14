2b9e445915aec32d68fd06c289f7b843c53eed09
Suyog Shinde <Suyog Shindesuyog.shinde@appliedaiconsulting.com>
2023-08-14 10:26:29 +0530

    added git configuration

--- src/main/java/com/example/configserver/ConfigServerApplication.java
+++ src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -6,18 +6,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-/*
+
 public class ConfigserverApplication {
 
         public static void main(String[] args) {
                 SpringApplication.run(ConfigserverApplication.class, args);
         }
 
-}
-*/
-
-public class ConfigServer {
-  public static void main(String[] args) {
-    SpringApplication.run(ConfigServer.class, args);
-  }
 }
